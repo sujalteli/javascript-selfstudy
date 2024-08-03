c31ef170e838364edd68236f4c65d850b7509e6f
Sujal teli <sujalteli55@gmail.com>
2024-08-03 04:45:47 +0000

    done

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -1,5 +1,5 @@
 let score = false
-
+ 
 console.log(typeof score);
 console.log(typeof (score));
 
@@ -11,4 +11,17 @@ console.log(valueInNumber);
 // "33" => 33
 // "33abc" => NaN (Not an Number)
 // true => 1; false => 0
+
+let isLoggedIn = 0
+let booleanIsisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsisLoggedIn);
+
+// 1 => true; 0 => false
+// "" => empty string always false
+// "sujal" => string with any value or name always true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
 
