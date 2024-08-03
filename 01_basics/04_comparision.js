054d2c91430c5cd3ca0821058aaa73e8e1479de7
Sujal teli <sujalteli55@gmail.com>
2024-08-03 06:53:48 +0000

    done

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,29 @@
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 == 1);
+console.log(2 != 1);
+
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+/*
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+// most of the time wee avoid this type of comparision bec it can make difficulty for us to undersatnd. 
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+*/
+
+
+// ===  "strict check"
+
+console.log("2" === 2);
+
